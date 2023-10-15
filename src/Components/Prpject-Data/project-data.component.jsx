57eb918d028817c1026fea8b05cfdd31732c7bd0
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2023-10-15 15:10:04 +0100

    added new project

--- src/Components/Prpject-Data/project-data.component.jsx
+++ src/Components/Prpject-Data/project-data.component.jsx
@@ -8,7 +8,18 @@ import {
 } from "./project-data.styles";
 const projects = [
   {
-    id: 1,
+    id: 16,
+    title: "React Quiz",
+    imageUrl:
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaA_RbLSjtZLZsrwkRL7Kg9RuVuUVktY1S7gbI2ElA3wopgd8f-MmDFaP8ptUfEh6ICgI-iTjLEQ7StVI4oYTGew9wZN=s1600",
+    description:
+      "Used languages: React JS and CSS. This React Quiz made in React. Purpose of this project to get more familiar with Reducer. Created with create-react-app, designed in CSS. The focus was on Reducer. It took 3 weeks to build this page.",
+    language: "React",
+    source: "https://github.com/Attila-Kis-Ivan/Qiuz",
+    demo: "https://tdb-react-quiz.netlify.app/",
+  },
+  {
+    id: 15,
     title: "Portfolio Page",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-zZVLwlEJGg_PdFicLc7dqP_CH8RplHP361FkhNaRvQw_VFgINSxCWbBN3Gvi_V7bEcYkYQrLrvBvMbmf3kWEMf-I9JEw=s1600",
@@ -19,7 +30,7 @@ const projects = [
     demo: "https://tdb-coding.com/",
   },
   {
-    id: 2,
+    id: 14,
     title: "TDB-Clothing-2.0",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-zgTglkWlQ_-lNpUHqiiB3X2CMGR6GAjktFZn9oui6so0FLInXPRcC935ve06qA1WU3GYVx83Uwr14MbGF09nPuJArw=s1600",
@@ -30,7 +41,7 @@ const projects = [
     demo: "https://tdb-clothing.netlify.app/",
   },
   {
-    id: 3,
+    id: 13,
     title: "FoodToGo",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-xFAlcSrptjtjSOnyIlzE_XeAEg57gbtoQjTCk7eujxA6Wmy8ojzRUC6HxLTwXLGQ8kyyWwGuQcT4qeuNTbYo8QiAl5=s1600",
@@ -41,7 +52,7 @@ const projects = [
     demo: "React Native project",
   },
   {
-    id: 4,
+    id: 12,
     title: "Face-Recognition-App",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-zD-S1rlhGwiFAiyeEy03_vtVMxFIoS95ENbXwfSePMoBOXjlkbKOJXSU59wgCOX0tVrZv47bzAAGDy85zPf52KvBcIgw=s1600",
@@ -52,7 +63,7 @@ const projects = [
     demo: "https://face-recognition-app-tcd8.onrender.com/",
   },
   {
-    id: 5,
+    id: 11,
     title: "Kanban Board",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-xTCscsEHs4SwaZc4sVGYDnGPELc7N1FZlGTSyTWuswP4Z-zlHh1wj_L1MYFkTXccyVZWQzrlkf53j5X2oxjNnPI9VPUg=s1600",
@@ -63,7 +74,7 @@ const projects = [
     demo: "https://attila-kis-ivan.github.io/Drag-and-Drop/",
   },
   {
-    id: 6,
+    id: 10,
     title: "Ping-Pong",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-xHBz-f2gMQzUE3LLs_NzHr3YDBGuYdd5aQ1e6vW54rNlx5-DQAl1EGDO7HaB9_W_l6czfRv1BDreVecLcmTIRlXVTeaQ=s1600",
@@ -74,7 +85,7 @@ const projects = [
     demo: "https://attila-kis-ivan.github.io/Ping-Pong/",
   },
   {
-    id: 7,
+    id: 9,
     title: "NASA-API-Pictures",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-yw9-erSZI2spBITbqRI9K_cY5PhQ7IX6bHygvkHO9v8KLSmxXnEefut7bcB2vmyJqPGxcqr5BNnTfA4ggrxi8qQL2VVQ=s1600",
@@ -97,7 +108,7 @@ const projects = [
   },
 
   {
-    id: 9,
+    id: 7,
     title: "Math Game",
     imageUrl:
       "https://drive.google.com/uc?export=download&id=1dhMOPpqypCDQwsn_N8aKWnmn2HvY4mJK",
@@ -109,7 +120,7 @@ const projects = [
   },
 
   {
-    id: 10,
+    id: 6,
     title: "Music Player",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-wvNaevTlPGy5wHg7xyksdCvi-uMSXiyTep8B9mXdKAJfhnpDK9oeOWb9PahzVFn6kVxJRX_woQnzVbcryLy5uLWXcv=s1600",
@@ -120,7 +131,7 @@ const projects = [
     demo: "https://attila-kis-ivan.github.io/Music-Player/",
   },
   {
-    id: 11,
+    id: 5,
     title: "Bookmark App",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-xrtVfYorZId8091nwf8DquF0Hii8v8KEnmq12yGyNSzDjTXWCbzcRv68IXn3J-5aK0yofKbPw7gx7hT0qQFJs7LilO=s1600",
@@ -132,7 +143,7 @@ const projects = [
   },
 
   {
-    id: 12,
+    id: 4,
     title: "Landing Page",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-zyxJ-3Ob-fdxhhWHXsnfb5Ad21_VFoiPlqrByMa3b_ioeKsak0YtN4uiuwyihZMYB3TqQZ2srLXJSOBqIvHjxVqotW=s1600",
@@ -144,7 +155,7 @@ const projects = [
   },
 
   {
-    id: 13,
+    id: 3,
     title: "Quote Generator",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-xnuzeDJWeETAhjs0mvOEwkLpSyAeSGLmDJPt4hgYHaZ450M8pcMg4qdbTu6cUNkMGclOcehAjAvwnasw66W-A_M2aWVA=s1600",
@@ -155,7 +166,7 @@ const projects = [
     demo: "https://attila-kis-ivan.github.io/Quote-Generator/",
   },
   {
-    id: 14,
+    id: 2,
     title: "Infinite Scroll",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-zcjtoxDxrDpMCaZ7PYBJ8ma5Gx119bUnp5jZdcRKE_rkNFTHT7zcTKii-KuYMuztsSgmQgJgj2M8QQ--EgtqTrpl_R=s1600",
@@ -166,7 +177,7 @@ const projects = [
     demo: "https://attila-kis-ivan.github.io/Infinite-Scroll/",
   },
   {
-    id: 15,
+    id: 1,
     title: "CSS background color generator",
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AITFw-wJETU9OfUW9akS7v-B02V1BW25u5P_AkJJJx0eMMKr_PJNTgBs2ASA2-p-hZYg3JgeirikDJiLz_2rLPkDwuBMv7PhIA=s1600",
