39771f8350e68a1b6877d3b114d0ada4eea2b96c
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2023-10-27 21:53:44 +0100

    added project photos

--- src/Components/Prpject-Data/project-data.component.jsx
+++ src/Components/Prpject-Data/project-data.component.jsx
@@ -10,6 +10,7 @@ const projects = [
   {
     id: 16,
     title: "React Quiz",
+
     imageUrl:
       "https://lh3.googleusercontent.com/drive-viewer/AK7aPaA_RbLSjtZLZsrwkRL7Kg9RuVuUVktY1S7gbI2ElA3wopgd8f-MmDFaP8ptUfEh6ICgI-iTjLEQ7StVI4oYTGew9wZN=s1600",
     description:
@@ -22,7 +23,7 @@ const projects = [
     id: 15,
     title: "Portfolio Page",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-zZVLwlEJGg_PdFicLc7dqP_CH8RplHP361FkhNaRvQw_VFgINSxCWbBN3Gvi_V7bEcYkYQrLrvBvMbmf3kWEMf-I9JEw=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaBr3IgihJlEZhXqwE9K2d33jfyxG_PhJv1zup6usoBMqnV510fzj0SNb4mFfalKMrkruvA9JekscHo5a5q634lWun6YJQ=s1600",
     description:
       "Used languages: React JS, Styled-Components. This portfolio site made in React. Purpose of this project obviously to have a portfolio page and practicing React JS. Created with create-react-app, designed in styled-components. It took 4 weeks to build this page.",
     language: "React",
@@ -33,7 +34,7 @@ const projects = [
     id: 14,
     title: "TDB-Clothing-2.0",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-zgTglkWlQ_-lNpUHqiiB3X2CMGR6GAjktFZn9oui6so0FLInXPRcC935ve06qA1WU3GYVx83Uwr14MbGF09nPuJArw=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaBx14TzidFA0FQNXVSSWEAZJAznPcfyNhdb15xHy-v8UTHktH4JIUXtDBXsEah2jy24JzsBJkqXrURRU2FaWjlUpuui=s1600",
     description:
       "Used languages: React JS. The purpose of this e-commerce shop project was to learn about React JS in depth. Created with create-react-app, and I used hooks and redux. Designed in styled-components. It took 6 months to build this page",
     language: "React",
@@ -44,7 +45,7 @@ const projects = [
     id: 13,
     title: "FoodToGo",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-xFAlcSrptjtjSOnyIlzE_XeAEg57gbtoQjTCk7eujxA6Wmy8ojzRUC6HxLTwXLGQ8kyyWwGuQcT4qeuNTbYo8QiAl5=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaBJEbwmMc7XFB73VgP6p_gWQOC73yY-5Lo7-BB5ozA8e62Z6yPXOVqeaJkGzMjBsw5J3Ec7kBf6nBYoXDUTGJmpuUS4=s1600",
     description:
       "Used languages: React Native. The purpose of this Restaurant app project was to learn Native in depth. Used expo, hooks, React Native map, 3rd parties APIs, IOS & Android simulator. Designed in styled-components. It took 3 months to build this page.",
     language: "React Native",
@@ -55,7 +56,7 @@ const projects = [
     id: 12,
     title: "Face-Recognition-App",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-zD-S1rlhGwiFAiyeEy03_vtVMxFIoS95ENbXwfSePMoBOXjlkbKOJXSU59wgCOX0tVrZv47bzAAGDy85zPf52KvBcIgw=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaCgbUfiGYB3_fYyatP-10lol5fyClc8p1K9GgudTK8p9N51cjbDAjHeX7SmEIW7A7HeswocRk7G35bFyF3L12iCW-3lFQ=s1600",
     description:
       "Used languages: React JS and Express JS. The purpose of this Face Recognition app project was to leran React and working with Clarifai AI API. It took 6 months to build this page.",
     language: "React, Node JS",
@@ -66,7 +67,7 @@ const projects = [
     id: 11,
     title: "Kanban Board",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-xTCscsEHs4SwaZc4sVGYDnGPELc7N1FZlGTSyTWuswP4Z-zlHh1wj_L1MYFkTXccyVZWQzrlkf53j5X2oxjNnPI9VPUg=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaAnFEkwpPYs-Ex4Fd0pKdwDxqhF4S38RFDPDSSNhnmFsJxo7eiso4NqFrASPccMXogVZal7mHbCZFoSRBlY7dXNbF8X=s1600",
     description:
       "Used languages: javascript, HTML5 and CSS. The purpose of this Kanban Board project was to learn javascript, local storage, drag and drop, and DOM manipulation in depth. It took 3 months to build this page",
     language: "javascript, HTML5 and CSS",
@@ -77,7 +78,7 @@ const projects = [
     id: 10,
     title: "Ping-Pong",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-xHBz-f2gMQzUE3LLs_NzHr3YDBGuYdd5aQ1e6vW54rNlx5-DQAl1EGDO7HaB9_W_l6czfRv1BDreVecLcmTIRlXVTeaQ=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaCJaz2eiIWYUeJxKlNL7HE1M0U3Cth2gy7oi__luIN4r9HplRVsj7i1cavLcEwK7OYqG3vhGt8GCSZYd7uZPwsn_EZTSQ=s1600",
     description:
       "Used languages: javascript, HTML5 and CSS. The purpose of this Ping-Pong game project was to learn javascript, canvas and DOM manipulation in depth. It took 3 months to build this page.",
     language: "javascript, CSS",
@@ -88,7 +89,7 @@ const projects = [
     id: 9,
     title: "NASA-API-Pictures",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-yw9-erSZI2spBITbqRI9K_cY5PhQ7IX6bHygvkHO9v8KLSmxXnEefut7bcB2vmyJqPGxcqr5BNnTfA4ggrxi8qQL2VVQ=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaAqoNEBtj_ESoqinIs2XUMaMOUPkg0qJ_qVlnIJHihJdtRlHMQ4tkn1Q9nsLPnFZ7han495nkJAwqS88ddo4wHSxTkLeg=s1600",
     description:
       "Used languages: javascript, HTML5 and CSS. The purpose of this NASA-API-Pictures project was to learn and practice javascript, loacal store, DOM manipulation and working with 3rd party API. It took 6 weeks to build this page.",
     language: "javascrip, HTML5 and CSS",
@@ -99,7 +100,7 @@ const projects = [
     id: 8,
     title: "Calculator",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-xqP9e8JPY_4nH1ajmGTx_YKsReIBFeT0CJhOknuN_mjatSKBIdBB8p3lbtAEWtbzOzyqk07N6C1PVC-zyXHlQywFOtBw=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaBY-0l8fSyPrbB7lanhE8MOabFuoiSXGRMMR0t7rk7p7UXBUtFMDAMMmzlEUI37i0Jpc2xiBbQQBIwAnA1cii3LfI8P6A=s1600",
     description:
       "Used languages: javascript, HTML5 and CSS. The purpose of this Calculator project was to learn and practice javascript DOM manipulation. It took 7 weeks to build this page.",
     language: "javascript, HTML5 and CSS",
@@ -111,7 +112,7 @@ const projects = [
     id: 7,
     title: "Math Game",
     imageUrl:
-      "https://drive.google.com/uc?export=download&id=1dhMOPpqypCDQwsn_N8aKWnmn2HvY4mJK",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaCPGjHedNnR_k8kUl7nuLLAc54jLMtu4e5RlsX7wSpS6zq29JSAneuqXzwL0uFM-Iy9l8xMNee21Nl4IchsuydBmq3dbw=s1600",
     description:
       "Used languages: javascript, HTML5 and CSS. The purpose of this Math Game project was to extend and challenge my javascript knowledge and DOM manipulation. It took 3 months to build this page.",
     language: "javascript, HTML5 and CSS",
@@ -123,7 +124,7 @@ const projects = [
     id: 6,
     title: "Music Player",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-wvNaevTlPGy5wHg7xyksdCvi-uMSXiyTep8B9mXdKAJfhnpDK9oeOWb9PahzVFn6kVxJRX_woQnzVbcryLy5uLWXcv=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaAS2r5lWcgiagh4EE8d2FSDjCjIm6tSn_6T40esJrTI2Ww-rQeAnctCGlkzs-GRgKDdmxVJY8iEyqZheJBRUpC-Tf1k=s1600",
     description:
       "Used languages: javascript, HTML5 and CSS. The purpose of this Music Player project was to extend and challenge my javascript knowledge (calculate progress bar etc...) and DOM manipulation. It took 2 months to build this page.",
     language: "javascript, HTML5 and CSS",
@@ -134,7 +135,7 @@ const projects = [
     id: 5,
     title: "Bookmark App",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-xrtVfYorZId8091nwf8DquF0Hii8v8KEnmq12yGyNSzDjTXWCbzcRv68IXn3J-5aK0yofKbPw7gx7hT0qQFJs7LilO=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaCWZlyniwzavIrrl0ENmA-jO7RX8q4Lp_6-ZiT7WCQiWPi3twLvv8DRItd_hBygSzNRr1VXNwzFuycEGaHJC7sXTBDU=s1600",
     description:
       "Used languages: javascript, HTML5 and CSS. The purpose of this Bookmark project was to learn and practice javascript and DOM manipulation. It took 2.5 months to build this page.",
     language: "javascript, HTML5 and CSS",
@@ -146,7 +147,7 @@ const projects = [
     id: 4,
     title: "Landing Page",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-zyxJ-3Ob-fdxhhWHXsnfb5Ad21_VFoiPlqrByMa3b_ioeKsak0YtN4uiuwyihZMYB3TqQZ2srLXJSOBqIvHjxVqotW=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaC2XsqH2rsPwddXiLkTREPIoVtsr73eFP_S-Xv-UlIGRkaUPkNWDd8A6w1uHTEus0lJKRKYzUhbO6wnq0ixkVbd8RgI=s1600",
     description:
       "Used languages: javascript, HTML5 and CSS. The purpose of this Landing page project was to learn and practice HTML5, CSS and a bit of javascript for DOM manipulation.",
     language: "javascript, HTML5 and CSS",
@@ -158,7 +159,7 @@ const projects = [
     id: 3,
     title: "Quote Generator",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-xnuzeDJWeETAhjs0mvOEwkLpSyAeSGLmDJPt4hgYHaZ450M8pcMg4qdbTu6cUNkMGclOcehAjAvwnasw66W-A_M2aWVA=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaDUB3Hskn7ssL2wNUqi3wrDApZMDtqGtqDudgnSXL7ZpBrtAIOgQ-psiA6kM0EJAbgPVlDKASn-kM_8uyMdaZ1U3y3oqg=s1600",
     description:
       "Used languages: javascript, HTML5 and CSS. The purpose of this Quote Generator project was to learn and practice javascript and DOM manipulation (such as, fetching datad etc..). It took 5 weeks to build this page.",
     language: "javascript, HTML5 and CSS",
@@ -169,7 +170,7 @@ const projects = [
     id: 2,
     title: "Infinite Scroll",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-zcjtoxDxrDpMCaZ7PYBJ8ma5Gx119bUnp5jZdcRKE_rkNFTHT7zcTKii-KuYMuztsSgmQgJgj2M8QQ--EgtqTrpl_R=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaCuzhX3LrnGo5Qts7hWXujG1ZxBHT9ZpKRkHdyYa_vrecH4EfkIa7OrJo0sl68xS-k8vutRUCyAwpfgAdS4ii_07zHc=s1600",
     description:
       "Used languages: javascript, HTML5 and CSS. The purpose of this Infinite Scroll project was to learn and practice javascript and working with picture API (such as, fetching datad etc..). It took 3 weeks to build this page.",
     language: "javascript, HTML5 and CSS",
@@ -180,7 +181,7 @@ const projects = [
     id: 1,
     title: "CSS background color generator",
     imageUrl:
-      "https://lh3.googleusercontent.com/drive-viewer/AITFw-wJETU9OfUW9akS7v-B02V1BW25u5P_AkJJJx0eMMKr_PJNTgBs2ASA2-p-hZYg3JgeirikDJiLz_2rLPkDwuBMv7PhIA=s1600",
+      "https://lh3.googleusercontent.com/drive-viewer/AK7aPaCXq5zPRxzGFfSHpBUSazTNN-fAyM4TDh1luQFjALgNnf9c-GUrAWpAjFLQxNXlgfRD6AWiQH-1vOsPfdT_x3eyrrF3dA=s1600",
     description:
       "Used languages: javascript, HTML5 and CSS. The purpose of this Background Color Generator project was to learn and practice javascript and DOM manipulation. It took 2 weeks to build this page.",
     language: "javascript, HTML5 and CSS",
